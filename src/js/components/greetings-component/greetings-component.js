966ef37e4bcd7ddf2699d1482d7bb5b909daa686
Jonathan Flores <jf223mu@student.lnu.se>
2023-08-28 15:37:05 +0200

    Add styling

--- src/js/components/greetings-component/greetings-component.js
+++ src/js/components/greetings-component/greetings-component.js
@@ -6,15 +6,51 @@ template.innerHTML = `
       display: flex;
       flex-direction: column;
       gap: 1rem;
+      font-family: -apple-system, BlinkMacSystemFont, "Segoe UI", Roboto, "Helvetica Neue", Arial, sans-serif;
+      max-width: 400px;
+      margin: 0 auto;
+      padding: 2rem;
+      box-shadow: 0 2px 10px rgba(0,0,0,0.1);
+      border-radius: 8px;
+    }
+    .inputForm label {
+      font-weight: 600;
+    }
+    .inputForm input, .inputForm button {
+      padding: 0.5rem 1rem;
+      border: 1px solid #ccc;
+      border-radius: 6px;
+      outline: none;
+      font-size: 1rem;
+      transition: border-color 0.3s;
+    }
+    .inputForm input:focus {
+      border-color: #007aff;
+    }
+    .inputForm button {
+      cursor: pointer;
+      background-color: #007aff;
+      color: white;
+      border: none;
+      margin-top: 1rem;
+      transition: background-color 0.3s;
+    }
+    .inputForm button:hover {
+      background-color: #005bb5;
+    }
+    .greeting {
+      margin-top: 1rem;
+      font-size: 1.2rem;
+      text-align: center;
     }
   </style>
 
-  <div class="inputForm">
+  <form class="inputForm">
     <label for="name">Name:</label>
     <input id="name" type="text">
-
     <button type="submit">Submit</button>
-  </div>
+  </form>
+  <div class="greeting"></div>
 `
 
 /**
@@ -22,6 +58,7 @@ template.innerHTML = `
  */
 class GreetingsComponent extends HTMLElement {
   /**
+   * 
    *
    */
   constructor () {
@@ -35,9 +72,19 @@ class GreetingsComponent extends HTMLElement {
    */
   connectedCallback () {
     const form = this.shadowRoot.querySelector('.inputForm')
+    const greeting = this.shadowRoot.querySelector('.greeting')
+
     form.addEventListener('submit', (e) => {
       e.preventDefault()
-      console.log('Form submitted')
+      const nameInput = this.shadowRoot.querySelector('#name')
+      const name = nameInput.value.trim()
+
+      if (name) {
+        greeting.textContent = `Hello ${name}, welcome back to programming.`
+        greeting.style.display = 'block'
+      } else {
+        greeting.style.display = 'none'
+      }
     })
   }
 }
