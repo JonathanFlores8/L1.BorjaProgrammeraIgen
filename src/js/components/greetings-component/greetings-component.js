fad3114a2bbfacdcc3f99b4b1080e05fa295adb2
Jonathan Flores <jf223mu@student.lnu.se>
2023-08-28 15:26:27 +0200

    Add submit form

--- src/js/components/greetings-component/greetings-component.js
+++ src/js/components/greetings-component/greetings-component.js
@@ -1,6 +1,45 @@
 const template = document.createElement('template')
+
 template.innerHTML = `
   <style>
+    .inputForm {
+      display: flex;
+      flex-direction: column;
+      gap: 1rem;
+    }
   </style>
 
-  <div class=""></div>`
+  <div class="inputForm">
+    <label for="name">Name:</label>
+    <input id="name" type="text">
+
+    <button type="submit">Submit</button>
+  </div>
+`
+
+/**
+ *
+ */
+class GreetingsComponent extends HTMLElement {
+  /**
+   *
+   */
+  constructor () {
+    super()
+    this.attachShadow({ mode: 'open' })
+    this.shadowRoot.appendChild(template.content.cloneNode(true))
+  }
+
+  /**
+   *
+   */
+  connectedCallback () {
+    const form = this.shadowRoot.querySelector('.inputForm')
+    form.addEventListener('submit', (e) => {
+      e.preventDefault()
+      console.log('Form submitted')
+    })
+  }
+}
+
+window.customElements.define('greetings-component', GreetingsComponent)
